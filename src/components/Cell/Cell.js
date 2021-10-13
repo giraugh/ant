629e2757a9ce2a368415c7a024a72de620fb4dac
Giraugh <ebreakey@gmail.com>
2021-10-13 14:15:42 +1100

    Improve mouse UX

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -5,6 +5,7 @@ import { GridCell } from './cellStyle'
 const Cell = ({ filled, isAnt, onClick }) =>
   <GridCell
     className={`${isAnt ? 'ant' : ''} ${filled ? 'filled' : 'empty'}`}
+    onMouseDown={onClick}
     onMouseEnter={e => e.buttons > 0 ? onClick() : null} />
 
 export default Cell
